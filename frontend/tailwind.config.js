0ce5477f375253903867d5a4c8b94e4e31e113a9
SriNikethanP <srinikethan.ponna@gmail.com>
2024-11-21 13:32:36 +0530

    basic model

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,22 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          ...daisyUIThemes["black"],
+          primary: "rgb(127, 0, 255)",
+          secondary: "rgb(26, 26, 26)",
+        },
+      },
+    ],
+  },
+};
